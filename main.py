afb04d25e0e2cf4a9ef1d4a8eaa40d230320d4d2
Luciano Santos <lucianosantsojob@gmail.com>
2021-10-28 23:21:17 -0300

    Terceiro commit

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from tkinter import font
 class Calc:
 
     def __init__(self):
+
+        #FUNÇÃO PARA CENTRALIZAR O TEXTO DO TÍTULO
+        def center(e):
+            w = int(self.window.winfo_width() / 3.5) # Obtenha a largura da janela e dimensione-a (em pixels).
+            s = 'Notepad'.rjust(w//2)
+            self.window.title(s)
+        
         #JANELA
         self.window = Tk()
-        self.window.title("Calc")
+        self.window.bind("<Configure>", center)
         self.window.resizable(0, 0)
-        self.window.config(bg="#1d2f38")
+        self.window.config(bg="#8f8d8c")
 
         #DISPLAY
-        self.screen_numbers = Entry(self.window, font="arial 20 bold", bg="#e3e5e6", fg="white", width=21)
+        self.screen_numbers = Entry(self.window, font="arial 20 bold", bg="#e3e5e6", fg="black", width=21)
         self.screen_numbers.pack()
 
         #FRAME PARA OS BOTÕES
@@ -19,53 +26,53 @@ class Calc:
         self.frame.pack()
 
         #BOTÕES
-        self.button_1 = Button(self.frame, bg="orange", bd=0, text="1", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_1 = Button(self.frame, bg="#8f8d8c", bd=0, text="1", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("1"))
         
-        self.button_2 = Button(self.frame, bg="orange", bd=0, text="2", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_2 = Button(self.frame, bg="#8f8d8c", bd=0, text="2", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("2"))
 
-        self.button_3 = Button(self.frame, bg="orange", bd=0, text="3", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_3 = Button(self.frame, bg="#8f8d8c", bd=0, text="3", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("3"))
 
-        self.button_4 = Button(self.frame, bg="orange", bd=0, text="4", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_4 = Button(self.frame, bg="#8f8d8c", bd=0, text="4", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("4"))
 
-        self.button_5 = Button(self.frame, bg="orange", bd=0, text="5", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_5 = Button(self.frame, bg="#8f8d8c", bd=0, text="5", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("5"))
 
-        self.button_6 = Button(self.frame, bg="orange", bd=0, text="6", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_6 = Button(self.frame, bg="#8f8d8c", bd=0, text="6", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("6"))
 
-        self.button_7 = Button(self.frame, bg="orange", bd=0, text="7", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_7 = Button(self.frame, bg="#8f8d8c", bd=0, text="7", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("7"))
 
-        self.button_8 = Button(self.frame, bg="orange", bd=0, text="8", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_8 = Button(self.frame, bg="#8f8d8c", bd=0, text="8", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("8"))
 
-        self.button_9 = Button(self.frame, bg="orange", bd=0, text="9", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_9 = Button(self.frame, bg="#8f8d8c", bd=0, text="9", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("9"))
 
-        self.button_0 = Button(self.frame, bg="orange", bd=0, text="0", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_0 = Button(self.frame, bg="#8f8d8c", bd=0, text="0", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("0"))
 
-        self.button_add = Button(self.frame, bg="orange", bd=0, text="+", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_add = Button(self.frame, bg="#8f8d8c", bd=0, text="+", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("+"))
 
-        self.button_sub = Button(self.frame, bg="orange", bd=0, text="-", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_sub = Button(self.frame, bg="#8f8d8c", bd=0, text="-", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("-"))
 
-        self.button_div = Button(self.frame, bg="orange", bd=0, text="/", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_div = Button(self.frame, bg="#8f8d8c", bd=0, text="/", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("/"))
 
-        self.button_mult = Button(self.frame, bg="orange", bd=0, text="*", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        self.button_mult = Button(self.frame, bg="#8f8d8c", bd=0, text="*", fg="white", font="arial 20 bold", 
+        width=5, height=3, command= lambda: self.touch("*"))
 
         self.button_equ = Button(self.frame, bg="black", bd=0, text="=", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        width=5, height=3, command=self.total)
 
         self.button_clean = Button(self.frame, bg="red", bd=0, text="C", fg="white", font="arial 20 bold", 
-        width=5, height=3, command=None)
+        width=5, height=3, command=self.Clean)
 
         #GRID DOS BOTÕES
         self.button_1.grid(row=0, column=0)
@@ -86,5 +93,20 @@ class Calc:
         self.button_clean.grid(row=0, column=3)
 
         self.window.mainloop()
+
+    #FUNÇÃO PARA CRIAR COMANDOS NOS BOTÕES
+    def touch(self, num):
+        self.screen_numbers.insert(END, num)
+        
+    #FUNÇÃO PARA CRIAR O COMANDO DE "LIMPAR" NO BOTÃO "C"
+    def Clean(self):
+        self.screen_numbers.delete(0, END)
+
+    #FUNÇÃO PARA INSERIR O RESULTADO
+    def total(self):
+        t = eval(self.screen_numbers.get())
+        self.screen_numbers.delete(0, END)
+        self.screen_numbers.insert(0, str(t))
+
 
 Calc()
